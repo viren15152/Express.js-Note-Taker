4b5b890050e14198ea12795fbf2e1b1c8cd5c533
Viren <viren15152@gmail.com>
2023-12-11 22:52:38 +0000

    Completed the html.Routes.js file

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+// This section of my code will get /notes and return the notes.html page.
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
+});
+// This section of my code will get * - and return the index.html page.  
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
+}); 
+
+module.exports = router;
+// This section of my code is used to export the router object from a file so that it can be imported and used in oter files. 
