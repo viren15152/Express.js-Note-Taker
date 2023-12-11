93db4d3dfdb5d7268c7d0a1ff44b29ff8c4d9c16
Viren <viren15152@gmail.com>
2023-12-11 22:40:01 +0000

    Completed my apiroutes.js file

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -17,3 +17,30 @@ router.get(' /notes', (req, res) => {
     });
 });
 
+//This section of my code will create a new note. 
+router.post('/notes', (req, res) => {
+    //This section of my code will read exsisting notes from db.json
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const notes = JSON.parse(data);
+        newNote.id = Date.now(); 
+        notes.push(newNote);
+
+        //This section of my code will update the notes back to db.json
+        fs.writeFile(dbFilePath, JSON.stringify(notes, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Internal Server Error'});
+            }
+
+            res.json(newNote);
+        });
+    });
+});
+
+module.exports = router;
+// This section of my code is used to export the router object from a file so that it can be imported and used in oter files. 
